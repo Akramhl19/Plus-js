83102c2b9b564319505d85207119dcbb1eff6ae4
akramHL <akremhelal@takiacademyiteam.com>
2021-12-16 17:44:54 +0100

    adding js

---
+++ Different scope of Loop Structure .js
@@ -0,0 +1,13 @@
+/*
+  Loop Fo
+  r Advanced Example
+*/
+
+let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor", "iPhone"];
+let i = 0;
+
+for (;;) {
+    console.log(products[i]);
+    i++;
+    if (i === products.length) break;
+}
---
+++ Number.js
@@ -0,0 +1,10 @@
+console.log(1000000);
+console.log(1_000_000);
+console.log(1e6);
+console.log(10 ** 6);
+console.log(10 * 10 * 10 * 10 * 10 * 10);
+console.log(1000000.0);
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_VALUE + 23434343434);
